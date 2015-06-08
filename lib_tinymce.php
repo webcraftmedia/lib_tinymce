e8b11f1d77cc3aa4187162dae17ca95211c2e567
rylon <rylon@web.de>
2015-06-08 17:44:51 +0200

    updated tinymce to 4.1.10 (2015-05-05)

--- lib_tinymce.php
+++ lib_tinymce.php
@@ -6,5 +6,5 @@ class lib_tinymce extends \LIB\lib_js{
     public static function js_path(){
         return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/tinymce/lib/tinymce.min.js');}
     public static function version(){
-        return '4.0.6 (2013-09-12)';}
+        return '4.1.10 (2015-05-05)';}
 }
