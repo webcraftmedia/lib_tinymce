6a3078b3f22b6d2189fdc3d5ddec49365120bc39
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-19 21:49:45 +0100

    path fix

--- lib_tinymce.php
+++ lib_tinymce.php
@@ -4,7 +4,7 @@ class lib_tinymce extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/tinymce/lib/tinymce.min.js');}
+        return new \SYSTEM\PLIB('/tinymce/lib/tinymce.min.js');}
     public static function version(){
         return '4.2.5';}
 }
